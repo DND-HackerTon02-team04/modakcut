f02fd50617c39cd842f14d4718db050487e76e1c
leeseojune <sung07288346@gmail.com>
2021-11-20 03:31:20 +0900

    ⚡ :: post /room/{room_id}

--- src/main/java/io/dnd/modakcut/domain/room/presentation/RoomController.java
+++ src/main/java/io/dnd/modakcut/domain/room/presentation/RoomController.java
@@ -1,7 +1,9 @@
 package io.dnd.modakcut.domain.room.presentation;
 
 import io.dnd.modakcut.domain.room.presentation.dto.response.MediaResponse;
+import io.dnd.modakcut.domain.room.presentation.dto.response.MediumUrlResponse;
 import io.dnd.modakcut.domain.room.presentation.dto.response.PostRoomResponse;
+import io.dnd.modakcut.domain.room.service.PostMediumService;
 import io.dnd.modakcut.domain.room.service.PostRoomService;
 import io.dnd.modakcut.domain.room.service.QueryRoomMediaService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/room")
@@ -21,6 +25,7 @@ public class RoomController {
 
 	private final PostRoomService postRoomService;
 	private final QueryRoomMediaService queryRoomMediaService;
+	private final PostMediumService postMediaService;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -31,6 +36,12 @@ public class RoomController {
 	@GetMapping("/{room_id}")
 	public MediaResponse queryRoomMedia(@PathVariable("room_id") String roomId) {
 		return queryRoomMediaService.execute(roomId);
+	}
+
+	@PostMapping("/{room_id}")
+	public MediumUrlResponse postMedium(@PathVariable("room_id") String roomId,
+			@RequestPart("file") MultipartFile multipartFile) {
+		return postMediaService.execute(roomId, multipartFile);
 	}
 
 }
