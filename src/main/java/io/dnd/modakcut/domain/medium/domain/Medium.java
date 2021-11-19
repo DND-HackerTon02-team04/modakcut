132e95d15e3dc7a3ab352037f928c6344aff1486
leeseojune <sung07288346@gmail.com>
2021-11-20 03:30:40 +0900

    🐛 :: 네이밍 변경

--- src/main/java/io/dnd/modakcut/domain/medium/domain/Medium.java
+++ src/main/java/io/dnd/modakcut/domain/medium/domain/Medium.java
@@ -26,7 +26,7 @@ public class Medium {
 	private String id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "group_id")
-	private Room group;
+	@JoinColumn(name = "room_id")
+	private Room room;
 
 }
