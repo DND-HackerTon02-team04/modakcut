eb88a68f5ff066816747be30077655377b37b22b
leeseojune <sung07288346@gmail.com>
2021-11-20 03:21:02 +0900

    ⚡ :: getter

--- src/main/java/io/dnd/modakcut/domain/medium/domain/Medium.java
+++ src/main/java/io/dnd/modakcut/domain/medium/domain/Medium.java
@@ -10,10 +10,12 @@ import io.dnd.modakcut.domain.room.domain.Room;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 
+@Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
