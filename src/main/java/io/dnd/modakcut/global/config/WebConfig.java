ae76ef79d02d80ef23cac8858827b57923088437
leeseojune <sung07288346@gmail.com>
2021-11-20 02:50:40 +0900

    📑 :: WebConfig

---
+++ src/main/java/io/dnd/modakcut/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package io.dnd.modakcut.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedHeaders("*")
+				.allowedMethods("GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH");
+	}
+	
+}
