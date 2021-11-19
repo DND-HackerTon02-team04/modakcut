523a012b5bc1d9505d19dd46103d1f1f9522e58e
leeseojune <sung07288346@gmail.com>
2021-11-20 01:50:51 +0900

    📑 :: Result Entity

---
+++ src/main/java/io/dnd/modakcut/domain/result/domain/Result.java
@@ -0,0 +1,28 @@
+package io.dnd.modakcut.domain.result.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import io.dnd.modakcut.domain.room.domain.Room;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name = "tbl_result")
+public class Result {
+
+	@Id
+	private String id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "group_id")
+	private Room group;
+
+}
