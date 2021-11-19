1a8c05bca46a0026c2469421b519b1c649525ddb
leeseojune <sung07288346@gmail.com>
2021-11-20 00:47:45 +0900

    📑 :: init

---
+++ src/main/java/io/dnd/modakcut/ModakcutApplication.java
@@ -0,0 +1,13 @@
+package io.dnd.modakcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModakcutApplication.class, args);
+	}
+
+}
