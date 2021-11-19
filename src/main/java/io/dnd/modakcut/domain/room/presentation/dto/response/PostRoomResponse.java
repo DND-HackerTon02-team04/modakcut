007840874fe0dff19524229b8c9d1cbaf03e4caf
leeseojune <sung07288346@gmail.com>
2021-11-20 03:17:18 +0900

    📑 :: PostRoomResponse Dto

---
+++ src/main/java/io/dnd/modakcut/domain/room/presentation/dto/response/PostRoomResponse.java
@@ -0,0 +1,12 @@
+package io.dnd.modakcut.domain.room.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostRoomResponse {
+
+	private final String roomCode;
+
+}
