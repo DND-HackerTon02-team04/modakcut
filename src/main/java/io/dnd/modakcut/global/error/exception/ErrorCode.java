8bdce9d95fff171335a4d24977f6eb1740f8e31f
leeseojune <sung07288346@gmail.com>
2021-11-20 02:41:36 +0900

    📑 :: ErrorCode

---
+++ src/main/java/io/dnd/modakcut/global/error/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package io.dnd.modakcut.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = Shape.OBJECT)
+public enum ErrorCode {
+
+	FILE_EMPTY(400, "File is Empty"),
+	FILE_UPLOAD_FAIL(400, "File upload was fail.");
+
+	private int status;
+	private String message;
+
+}
